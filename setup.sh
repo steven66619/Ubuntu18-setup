b5ac4b391fafce74b3f3096065dabb987c72031d
TIBTHINK <38056175+TIBTHINK@users.noreply.github.com>
2020-05-10 05:35:08 -0400

    Update setup.sh
    added in stufferoo

--- setup.sh
+++ setup.sh
@@ -54,6 +54,14 @@ sudo apt-get install -y gimp scribus
 ## Games
 sudo apt-get install -y steam-installer
 
+## Music
+curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
+
+## Text Editor
+sudo snap install --classic code
+
 ## Disable Apport
 sudo sed -i 's/enabled=1/enabled=0/g' /etc/default/apport
 
